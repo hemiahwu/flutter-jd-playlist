4dca2e98bc132964b0c771a84964959e906fbd55
hemiahwu <309595700@qq.com>
2019-12-23 15:08:20 +0800

    lesson-1

---
+++ lib/page/index_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  IndexPage({Key key}) : super(key: key);
+
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("Hello JD_APP"));
+  }
+}
