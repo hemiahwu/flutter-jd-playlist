b5b5173aebaf524e813c066b591d39226a3e6ff9
hemiahwu <309595700@qq.com>
2019-12-25 15:21:41 +0800

    lesson-9

--- lib/page/index_page.dart
+++ lib/page/index_page.dart
@@ -3,6 +3,8 @@ import 'package:jd_app/page/home_page.dart';
 import 'package:jd_app/page/category_page.dart';
 import 'package:jd_app/page/cart_page.dart';
 import 'package:jd_app/page/user_page.dart';
+import 'package:jd_app/provider/bottom_navi_provider.dart';
+import 'package:provider/provider.dart';
 
 class IndexPage extends StatefulWidget {
   IndexPage({Key key}) : super(key: key);
@@ -12,47 +14,50 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("首页"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            title: Text("分类"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            title: Text("购物车"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text("我的"),
-          ),
-        ],
-        onTap: (index) {
-          // print(index);
-          setState(() {
-            _currentIndex = index;
-          });
+      bottomNavigationBar: Consumer<BottomNaviProvider>(
+        builder: (_, mProvider, __) {
+          return BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: mProvider.bottomNaviIndex,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("首页"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                title: Text("分类"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text("购物车"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text("我的"),
+              ),
+            ],
+            onTap: (index) {
+              // print(index);
+              mProvider.changeBottomNaviIndex(index);
+            },
+          );
         },
       ),
-      body: IndexedStack(
-        index: _currentIndex,
-        // 层布局控件 只显示一个
-        children: <Widget>[
-          HomePage(),
-          CategoryPage(),
-          CartPage(),
-          UserPage(),
-        ],
+      body: Consumer<BottomNaviProvider>(
+        builder: (_, mProvider, __) => IndexedStack(
+          index: mProvider.bottomNaviIndex,
+          // 层布局控件 只显示一个
+          children: <Widget>[
+            HomePage(),
+            CategoryPage(),
+            CartPage(),
+            UserPage(),
+          ],
+        ),
       ),
     );
   }
