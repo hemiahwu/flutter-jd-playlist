b324530095b588cac24142bdc5266fd38be3c35d
米斯特吴 <2330075867@qq.com>
2020-04-14 15:04:10 +0800

    1

--- lib/model/category_content_model.dart
+++ lib/model/category_content_model.dart
@@ -1,33 +1,10 @@
 class CategoryContentModel {
-  List<Data> data;
-
-  CategoryContentModel({this.data});
-
-  CategoryContentModel.fromJson(Map<String, dynamic> json) {
-    if (json['data'] != null) {
-      data = new List<Data>();
-      json['data'].forEach((v) {
-        data.add(new Data.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.data != null) {
-      data['data'] = this.data.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
-class Data {
   String title;
   List<Desc> desc;
 
-  Data({this.title, this.desc});
+  CategoryContentModel({this.title, this.desc});
 
-  Data.fromJson(Map<String, dynamic> json) {
+  CategoryContentModel.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     if (json['desc'] != null) {
       desc = new List<Desc>();
--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jd_app/model/category_content_model.dart';
 import 'package:jd_app/provider/category_page_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -27,7 +28,6 @@ class _CategoryPageState extends State<CategoryPage> {
               title: Text("分类"),
             ),
             body: Container(
-              color: Color(0xFFf7f7f7),
               child: Consumer<CategoryPageProvider>(
                 builder: (_, provider, __) {
                   // 加载动画
@@ -56,34 +56,110 @@ class _CategoryPageState extends State<CategoryPage> {
 
                   return Row(
                     children: <Widget>[
-                      Container(
-                        width: 90,
-                        child: ListView.builder(
-                            itemCount: provider.categoryNavList.length,
-                            itemBuilder: (context, index) {
-                              return InkWell(
-                                child: Container(
-                                    height: 50.0,
-                                    padding: const EdgeInsets.only(top: 15),
-                                    color: Color(0xFFF8F8F8),
-                                    child: Text(
-                                      provider.categoryNavList[index],
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          color: Color(0xFF333333),
-                                          fontWeight: FontWeight.w500),
-                                    )),
-                                onTap: () {
-                                  // print(index);
-                                  provider.loadCategoryContentData(index);
-                                },
-                              );
-                            }),
+                      // 左侧
+                      buildNavLeftContainer(provider),
+                      // 右侧
+                      Expanded(
+                        child: Stack(
+                          children: <Widget>[
+                            buildCategoryContent(provider.categoryContentList),
+                            provider.isLoading
+                                ? Center(
+                                    child: CupertinoActivityIndicator(),
+                                  )
+                                : Container()
+                          ],
+                        ),
                       )
                     ],
                   );
                 },
               ),
             )));
+  }
+
+  Container buildNavLeftContainer(CategoryPageProvider provider) {
+    return Container(
+      width: 90,
+      child: ListView.builder(
+          itemCount: provider.categoryNavList.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              child: Container(
+                  height: 50.0,
+                  padding: const EdgeInsets.only(top: 15),
+                  color: provider.tabIndex == index
+                      ? Colors.white
+                      : Color(0xFFF8F8F8),
+                  child: Text(
+                    provider.categoryNavList[index],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: provider.tabIndex == index
+                            ? Color(0xFFe93b3d)
+                            : Color(0xFF333333),
+                        fontWeight: FontWeight.w500),
+                  )),
+              onTap: () {
+                // print(index);
+                provider.loadCategoryContentData(index);
+              },
+            );
+          }),
+    );
+  }
+
+  Widget buildCategoryContent(List<CategoryContentModel> contentList) {
+    List<Widget> list = List<Widget>();
+
+    for (var i = 0; i < contentList.length; i++) {
+      list.add(Container(
+        height: 30.0,
+        margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+        child: Text(
+          "${contentList[i].title}",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+        ),
+      ));
+      List<Widget> descList = List<Widget>();
+      for (var j = 0; j < contentList[i].desc.length; j++) {
+        descList.add(InkWell(
+          child: Container(
+            width: 60.0,
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets${contentList[i].desc[j].img}",
+                  width: 50,
+                  height: 50,
+                ),
+                Text("${contentList[i].desc[j].text}")
+              ],
+            ),
+          ),
+          onTap: () {
+            // 前往商品列表页面
+          },
+        ));
+      }
+
+      list.add(Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          spacing: 7.0,
+          runSpacing: 10.0,
+          alignment: WrapAlignment.start,
+          children: descList,
+        ),
+      ));
+    }
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      child: ListView(
+        children: list,
+      ),
+    );
   }
 }
