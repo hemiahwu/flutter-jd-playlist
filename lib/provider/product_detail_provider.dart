912b7bed1ec8aef43675ba667e6dcd0e79d888fe
米斯特吴 <2330075867@qq.com>
2020-04-17 17:38:08 +0800

    34

--- lib/provider/product_detail_provider.dart
+++ lib/provider/product_detail_provider.dart
@@ -37,18 +37,14 @@ class ProductDetailProvider with ChangeNotifier {
 
   // 切换分期模式
   void changeBaitiaoSeleted(int index) {
-    List<Baitiao> list = [];
-    if (this.model.baitiao[index] == false) {
+    if (this.model.baitiao[index].select == false) {
       for (int i = 0; i < this.model.baitiao.length; i++) {
         if (i == index) {
           this.model.baitiao[i].select = true;
         } else {
           this.model.baitiao[i].select = false;
         }
-        list.add(this.model.baitiao[i]);
       }
-      this.model.baitiao.clear();
-      this.model.baitiao.addAll(list);
       notifyListeners();
     }
   }
